1bc56ea5155e62184180b75766ad52cabb7e4290
unknown <mohsinpeer22@gmail.com>
2021-12-31 00:20:16 +0530

    Creating a LinkedList

---
+++ LinkedListDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LinkedListOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Operations!");
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            //Display Operation
+            list.Display();
+           
+        }   
+    }
+}
